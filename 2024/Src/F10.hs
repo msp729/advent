8355d8cede34ed2542acf2c3768497539cdc0c8a
Nate Brown <nravinbrown@gmail.com>
2025-08-27 09:18:41 -0400

    finished day 7

--- 2024/Src/F10.hs
+++ 2024/Src/F10.hs
@@ -3,6 +3,7 @@
 
 module Src.F10 (calcDay) where
 
+import Control.Applicative (liftA3)
 import Data.IntSet (IntSet)
 import qualified Data.IntSet as IS
 import Data.List (elemIndex, findIndex, nub)
@@ -23,90 +24,96 @@ data Direction = N | S | E | W deriving (Show, Eq, Enum, Ord)
 
 -- | day 6
 day6 grid = do
-    let
-        h = length grid
-        w = length $ head grid
-        get (y, x) = grid !! y !! x
-        locations = [(y, x) | y <- [0 .. h - 1], x <- [0 .. w - 1]]
-        obstacles = filter ((== '#') . get) locations
-        Just iy = findIndex (elem '^') grid
-        Just ix = elemIndex '^' $ grid !! iy
-
-        move :: Direction -> (Int, Int) -> (Int, Int)
-        move N (y, x) = (y - 1, x)
-        move S (y, x) = (y + 1, x)
-        move E (y, x) = (y, x + 1)
-        move W (y, x) = (y, x - 1)
-
-        turn :: Direction -> Direction
-        turn N = E
-        turn E = S
-        turn S = W
-        turn W = N
-
-        update :: (Direction, (Int, Int)) -> (Direction, (Int, Int))
-        update (d, p) = let p' = move d p in if elem p' obstacles then (turn d, p) else (d, p')
-        inGrid (d, (y, x)) = y >= 0 && x >= 0 && y < h && x < w
-        guardPath :: [(Int, Int)]
-        guardPath = nub $ snd <$> takeWhile inGrid path'
-          where
-            path' = iterate update (N, (iy, ix))
-
-        possibleObstacles :: [(Int, Int)]
-        possibleObstacles = tail guardPath
-
-        update' :: (Int, Int) -> (Direction, (Int, Int)) -> (Direction, (Int, Int))
-        update' ob (d, p) = let p' = move d p in if elem p' obstacles || p' == ob then (turn d, p) else (d, p')
-        prospect :: (Int, Int) -> [(Direction, (Int, Int))]
-        prospect ob = iterate (update' ob) (N, (iy, ix))
-        -- returns one of the following, whichever is shorter:
-        --  - the longest prefix of the given list with no duplicates (longest prefix to a loop)
-        --  - the longest prefix of the given list s.t. all values evaluate to True under the predicate
-        -- the attached Bool is True if it's a loop prefix, False if it's a takeWhile
-        getPrefix :: (Eq a, Ord a) => (a -> Bool) -> [a] -> (Bool, [a])
-        getPrefix p = fmap reverse . helper p mempty
-          where
-            helper :: (Eq a, Ord a) => (a -> Bool) -> Set a -> [a] -> (Bool, [a])
-            helper p s [] = (False, [])
-            helper p s (x : xs)
-                | x `S.member` s = (True, [])
-                | p x = (x :) <$> helper p (S.insert x s) xs
-                | otherwise = (False, [])
-
-        loopObstacles =
-            [ ob
-            | ob <- possibleObstacles
-            , let pre = getPrefix inGrid (prospect ob)
-            , fst pre
-            ]
-
     print $ length guardPath
     print $ length loopObstacles
+  where
+    h = length grid
+    w = length $ head grid
+    get (y, x) = grid !! y !! x
+    locations = [(y, x) | y <- [0 .. h - 1], x <- [0 .. w - 1]]
+    obstacles = filter ((== '#') . get) locations
+    Just iy = findIndex (elem '^') grid
+    Just ix = elemIndex '^' $ grid !! iy
+
+    move :: Direction -> (Int, Int) -> (Int, Int)
+    move N (y, x) = (y - 1, x)
+    move S (y, x) = (y + 1, x)
+    move E (y, x) = (y, x + 1)
+    move W (y, x) = (y, x - 1)
+
+    turn :: Direction -> Direction
+    turn N = E
+    turn E = S
+    turn S = W
+    turn W = N
+
+    update :: (Direction, (Int, Int)) -> (Direction, (Int, Int))
+    update (d, p) = let p' = move d p in if elem p' obstacles then (turn d, p) else (d, p')
+    inGrid (d, (y, x)) = y >= 0 && x >= 0 && y < h && x < w
+    guardPath :: [(Int, Int)]
+    guardPath = nub $ snd <$> takeWhile inGrid path'
+      where
+        path' = iterate update (N, (iy, ix))
+
+    possibleObstacles :: [(Int, Int)]
+    possibleObstacles = drop 1 guardPath
+
+    update' :: (Int, Int) -> (Direction, (Int, Int)) -> (Direction, (Int, Int))
+    update' ob (d, p) = let p' = move d p in if elem p' obstacles || p' == ob then (turn d, p) else (d, p')
+    prospect :: (Int, Int) -> [(Direction, (Int, Int))]
+    prospect ob = iterate (update' ob) (N, (iy, ix))
+    -- returns one of the following, whichever is shorter:
+    --  - the longest prefix of the given list with no duplicates (longest prefix to a loop)
+    --  - the longest prefix of the given list s.t. all values evaluate to True under the predicate
+    -- the attached Bool is True if it's a loop prefix, False if it's a takeWhile
+    getPrefix :: (Eq a, Ord a) => (a -> Bool) -> [a] -> (Bool, [a])
+    getPrefix p = fmap reverse . helper p mempty
+      where
+        helper :: (Eq a, Ord a) => (a -> Bool) -> Set a -> [a] -> (Bool, [a])
+        helper p s [] = (False, [])
+        helper p s (x : xs)
+            | x `S.member` s = (True, [])
+            | p x = (x :) <$> helper p (S.insert x s) xs
+            | otherwise = (False, [])
+
+    loopObstacles = filter (fst . getPrefix inGrid . prospect) possibleObstacles
+
+type I7 = Int
+type S7 = IntSet
 
 -- | day 7
-day7 txt = do print part1
+day7 txt = do
+    print part1
+    print part2
   where
-    parser :: String -> (Int, Int, [Int])
+    parser :: String -> (I7, I7, [I7])
     parser s =
         let
             (stv : sfv : srest) = words s
-            tv, fv :: Int
+            tv, fv :: I7
             tv = read $ init stv
             fv = read sfv
-            rest :: [Int]
+            rest :: [I7]
             rest = read <$> srest
          in
             (tv, fv, rest)
 
-    combs :: IntSet -> Int -> IntSet
+    combs :: S7 -> I7 -> S7
     combs s x = liftA2 IS.union (IS.map (x *)) (IS.map (x +)) s
 
-    attemptline :: (Int, Int, [Int]) -> Maybe Int
+    combs' :: I7 -> S7 -> I7 -> S7
+    combs' t s x = liftA3 ((IS.union .) . IS.union) (IS.map (x *)) (IS.map (x +)) (IS.map (>< x)) s
+
+    attemptline, attempt' :: (I7, I7, [I7]) -> Maybe I7
     attemptline (tv, fv, rv) = if IS.member tv $ foldl' combs (IS.singleton fv) rv then Just tv else Nothing
+    attempt' (tv, fv, rv) = if IS.member tv $ foldl' (combs' tv) (IS.singleton fv) rv then Just tv else Nothing
+
+    (><) :: I7 -> I7 -> I7
+    a >< b = read (show a <> show b)
 
     parsed = parser <$> txt
-    attempted = attemptline <$> parsed
-    part1 = sum $ catMaybes attempted
+    part1 = sum $ catMaybes $ attemptline <$> parsed
+    part2 = sum $ catMaybes $ attempt' <$> parsed
 
 -- | day 8
 day8 = undefined
--- 2024/Src/Main.hs
+++ 2024/Src/Main.hs
@@ -13,6 +13,9 @@ main = do
         nums = args >>= maybe [] pure
         days = filter (liftA2 (&&) (<= 25) (>= 1)) nums
     mapM_ runDay days
+    F10.calcDay 7 $
+        lines
+            "190: 10 19\n3267: 81 40 27\n83: 17 5\n156: 15 6\n7290: 6 8 6 15\n161011: 16 10 13\n192: 17 8 14\n21037: 9 7 18 13\n292: 11 6 16 20"
     return ()
 
 runDay :: Int -> IO ()
